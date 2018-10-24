a0cfaa9395b3eeb945e337b3421972fe206874de
StefanoSQ <quaglia.stefano123@gmail.com>
2018-10-24 12:22:45 +0200

    silly comment

--- WindowsFormsApp1/WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Program.cs
@@ -16,6 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //sufgeuifwo
             Application.Run(new Form1())  ;
         } 
     }
